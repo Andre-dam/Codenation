cba39beef43e57550a98c73c61ef22f64d61a148
andre.junior <aldsj@cin.ufpe.br>
2020-04-30 00:25:13 -0300

    feat(java-8): adjust method position and refactor

--- java-8/src/main/java/com/challenge/desafio/CalculadorDeClasses.java
+++ java-8/src/main/java/com/challenge/desafio/CalculadorDeClasses.java
@@ -5,6 +5,7 @@ import com.challenge.annotation.Subtrair;
 import com.challenge.interfaces.Calculavel;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.util.Arrays;
 
@@ -20,23 +21,25 @@ public class CalculadorDeClasses implements Calculavel {
         return this.somarAtributos((Class) Subtrair.class, object);
     }
 
+    @Override
+    public BigDecimal totalizar(Object object) {
+        return this.somar(object).subtract(this.subtrair(object));
+    }
+
     private BigDecimal somarAtributos(Class<Annotation> annotationClass, Object object) {
         return Arrays.stream(object.getClass().getDeclaredFields())
                 .filter(field -> field.isAnnotationPresent(annotationClass))
                 .filter(field -> field.getType().equals(BigDecimal.class))
-                .map(field -> {
-                    try {
-                        field.setAccessible(true);
-                        return (BigDecimal) field.get(object);
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException("Wrapped checked exception", e);
-                    }
-                })
+                .map(field -> this.extrairAtributo(field, object))
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
-    @Override
-    public BigDecimal totalizar(Object object) {
-        return this.somar(object).subtract(this.subtrair(object));
+    private BigDecimal extrairAtributo(Field field, Object object) {
+        try {
+            field.setAccessible(true);
+            return (BigDecimal) field.get(object);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Wrapped checked exception", e);
+        }
     }
 }
