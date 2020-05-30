bf6e9ec20fa8d9f47d1cb9eb993e46d1c82bbde3
andre.junior <aldsj@cin.ufpe.br>
2020-05-30 01:39:04 -0300

    feat(CandidateController): user path parameters intead

--- java-11/src/main/java/com/challenge/endpoints/CandidateController.java
+++ java-11/src/main/java/com/challenge/endpoints/CandidateController.java
@@ -3,38 +3,43 @@ package com.challenge.endpoints;
 import com.challenge.dto.CandidateDTO;
 import com.challenge.endpoints.exceptions.ResourceNotFoundException;
 import com.challenge.mappers.CandidateMapper;
-import com.challenge.service.impl.CandidateService;
+import com.challenge.service.interfaces.CandidateServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("candidate")
 public class CandidateController {
     @Autowired
-    CandidateService candidateService;
+    CandidateServiceInterface candidateService;
 
     @Autowired
     CandidateMapper candidateMapper;
 
-    @GetMapping(value = "/candidate", params = {"userId", "companyId", "accelerationId"})
-    ResponseEntity<CandidateDTO> findById(@RequestParam Long userId, @RequestParam Long companyId, @RequestParam Long accelerationId) {
+    @GetMapping(value = "/{userId}/{accelerationId}/{companyId}")
+    ResponseEntity<CandidateDTO> findById(
+            @PathVariable Long userId,
+            @PathVariable Long accelerationId,
+            @PathVariable Long companyId) {
         return new ResponseEntity<>(candidateService.findById(userId, companyId, accelerationId)
                 .map(this.candidateMapper::map)
                 .orElseThrow(() -> new ResourceNotFoundException("Candidate")), HttpStatus.OK);
     }
 
-    @GetMapping(value = "/candidate", params = "companyId")
+    @GetMapping(params = "companyId")
     List<CandidateDTO> findByCompanyId(@RequestParam Long companyId) {
         return this.candidateMapper.map(candidateService.findByCompanyId(companyId));
     }
 
-    @GetMapping(value = "/candidate", params = "accelerationId")
+    @GetMapping(params = "accelerationId")
     List<CandidateDTO> findByAccelerationId(@RequestParam Long accelerationId) {
         return this.candidateMapper.map(candidateService.findByAccelerationId(accelerationId));
     }
--- java-11/src/main/java/com/challenge/endpoints/UserController.java
+++ java-11/src/main/java/com/challenge/endpoints/UserController.java
@@ -2,40 +2,37 @@ package com.challenge.endpoints;
 
 import com.challenge.endpoints.exceptions.ResourceNotFoundException;
 import com.challenge.entity.User;
-import com.challenge.service.impl.UserService;
+import com.challenge.service.interfaces.UserServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
-    UserService userService;
+    UserServiceInterface userService;
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/{id}")
     ResponseEntity<User> getById(@PathVariable Long id) {
         return new ResponseEntity<>(this.userService.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User")), HttpStatus.OK);
     }
 
-    @GetMapping("/user")
-    List<User> getByQueryParameters(@RequestParam Map<String,String> requestParams) {
-        String accelerationName = requestParams.get("accelerationName");
-        String companyId = requestParams.get("companyId");
+    @GetMapping(params = "accelerationName")
+    List<User> getByAccelerationName(@RequestParam String accelerationName) {
+        return userService.findByAccelerationName(accelerationName);
+    }
 
-        List<User> response;
-        if(accelerationName != null){
-            response = userService.findByAccelerationName(accelerationName);
-        }else{
-            response = userService.findByCompanyId(Long.valueOf(companyId));
-        }
-        return response;
+    @GetMapping(params = "companyId")
+    List<User> getByCompanyId(@RequestParam Long companyId) {
+        return userService.findByCompanyId(companyId);
     }
 }
